853c772b1f6cd340c31b92b39474f9e379284b9a
Shivam Singh <shivamsingh4458@gmail.com>
2024-02-07 15:29:03 +0530

    Updated Backend URL

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://mediumbackenddata.herokuapp.com/blogsdata/"
+export const BASE_URL = "https://shivam-medium-learn.onrender.com/blogsdata/"
 
 export const auth = "authorization"
 
